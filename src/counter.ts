1a44db652e0a1e2fa493c84aaff53cb4712840a2
rpire <rubenpire7@gmail.com>
2024-05-14 22:27:36 -0400

    Fix format for the src/ folder

--- src/counter.ts
+++ src/counter.ts
@@ -1,11 +1,11 @@
 const setupCounter = (element: HTMLButtonElement): void => {
-  let counter = 0
+  let counter = 0;
   const setCounter = (count: number): void => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
-  }
-  element.addEventListener('click', (): void => setCounter(counter + 1))
-  setCounter(0)
+    counter = count;
+    element.innerHTML = `count is ${counter}`;
+  };
+  element.addEventListener('click', (): void => setCounter(counter + 1));
+  setCounter(0);
 };
 
 export default setupCounter;
