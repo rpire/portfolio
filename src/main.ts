3f2c8cc8d15c6fbab188398ffb812a973556a5bc
rpire <rubenpire7@gmail.com>
2024-06-03 17:48:59 -0400

    Replace the TypeScript files in src/ and remove unnecessary media

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,7 @@
-import setupCounter from './counter.ts';
-import typescriptLogo from './typescript.svg';
-import viteLogo from '/vite.svg';
+import changeBGColor from './changeBGColor';
 import './style.scss';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`;
+const changeButton = document.getElementById('change-button');
+const changingElement = document.getElementById('changing-element');
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!);
+changeButton?.addEventListener('click', () => changeBGColor(changingElement));
