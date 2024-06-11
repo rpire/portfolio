04b4c205990d0c57769585eaac63462a18d3c737
rpire <rubenpire7@gmail.com>
2024-06-10 22:46:36 -0400

    Implement the renderProjectCards function in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,7 @@
+import projects from './data/projects';
+import renderProjectCards from './helpers/renderProjects';
 import './style.scss';
+
+const projectCardsContainer = document.getElementById('project-cards');
+
+if (projectCardsContainer) renderProjectCards(projectCardsContainer, projects);
