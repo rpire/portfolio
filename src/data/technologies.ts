49748752f71a87a8f697ecb598e39329799808fb
rpire <rubenpire7@gmail.com>
2024-06-13 15:22:59 -0400

    List technology icons in a technologies array
    List technology icons in a technologies array in the src/data/technologies.ts file.

---
+++ src/data/technologies.ts
@@ -0,0 +1,63 @@
+import amazonwebservices from '../assets/images/tech-icons/amazonwebservices.svg';
+import bootstrap from '../assets/images/tech-icons/bootstrap.svg';
+import css from '../assets/images/tech-icons/css.svg';
+import eslint from '../assets/images/tech-icons/eslint.svg';
+import express from '../assets/images/tech-icons/express.svg';
+import git from '../assets/images/tech-icons/git.svg';
+import githubactions from '../assets/images/tech-icons/githubactions.svg';
+import googlecloud from '../assets/images/tech-icons/googlecloud.svg';
+import heroku from '../assets/images/tech-icons/heroku.svg';
+import html from '../assets/images/tech-icons/html.svg';
+import javascript from '../assets/images/tech-icons/javascript.svg';
+import jest from '../assets/images/tech-icons/jest.svg';
+import mongodb from '../assets/images/tech-icons/mongodb.svg';
+import nestjs from '../assets/images/tech-icons/nestjs.svg';
+import nextdotjs from '../assets/images/tech-icons/nextdotjs.svg';
+import nodedotjs from '../assets/images/tech-icons/nodedotjs.svg';
+import npm from '../assets/images/tech-icons/npm.svg';
+import postgresql from '../assets/images/tech-icons/postgresql.svg';
+import react from '../assets/images/tech-icons/react.svg';
+import redux from '../assets/images/tech-icons/redux.svg';
+import ror from '../assets/images/tech-icons/ror.svg';
+import ruby from '../assets/images/tech-icons/ruby.svg';
+import sass from '../assets/images/tech-icons/sass.svg';
+import scrum from '../assets/images/tech-icons/scrum.svg';
+import stylelint from '../assets/images/tech-icons/stylelint.svg';
+import typescript from '../assets/images/tech-icons/typescript.svg';
+import ubuntu from '../assets/images/tech-icons/ubuntu.svg';
+import vite from '../assets/images/tech-icons/vite.svg';
+import vitest from '../assets/images/tech-icons/vitest.svg';
+
+const technologies: string[] = [
+  amazonwebservices,
+  bootstrap,
+  css,
+  eslint,
+  express,
+  git,
+  githubactions,
+  googlecloud,
+  heroku,
+  html,
+  javascript,
+  jest,
+  mongodb,
+  nestjs,
+  nextdotjs,
+  nodedotjs,
+  npm,
+  postgresql,
+  react,
+  redux,
+  ror,
+  ruby,
+  sass,
+  scrum,
+  stylelint,
+  typescript,
+  ubuntu,
+  vite,
+  vitest,
+];
+
+export default technologies;
